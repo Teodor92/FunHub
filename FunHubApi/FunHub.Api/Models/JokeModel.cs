e13d2146472f91ea34cc4aae0424e6450f369c10
Teodor92 <teodor.ivanov92@gmail.com>
2014-07-18 13:23:19 +0300

    Added fun hub code and moved api code to its own folder.

---
+++ FunHubApi/FunHub.Api/Models/JokeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace FunHub.Api.Models
+{
+    [DataContract]
+    public class JokeModel
+    {
+        [DataMember(Name = "postedBy")]
+        public string PostedBy { get; set; }
+
+        [DataMember(Name = "postDate")]
+        public DateTime PostDate { get; set; }
+
+        [DataMember(Name = "content")]
+        public string Content { get; set; }
+    }
+}
---
+++ FunHubApi/FunHub.Data/FunHubContext.cs
@@ -0,0 +1,20 @@
+﻿using FunHub.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunHub.Data
+{
+    public class FunHubContext : DbContext
+    {
+        public FunHubContext()
+            : base("FunHubDb")
+        {
+        }
+
+        public DbSet<Joke> Jokes { get; set; }
+    }
+}
